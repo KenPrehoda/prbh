ec5b37ab815e9cbc70a652ad94b783ec688267fc
Ken Prehoda <kenprehoda@gmail.com>
2023-07-17 13:44:37 -0700

    argument parsing

--- prbh.py
+++ prbh.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+import gzip
 import argparse
+from dataclasses import dataclass
 
+HYD_RES = {'A':0.05,'F':0.2,'I':0.2,'L':0.2,'M':0.25,'V':0.1,'W':0.1,'Y':0.1}
+BASIC_RES = {'R':0.1,'K':0.07,'H':0.04,'D':-0.06,'E':-0.06,'Y':0.0}
+M3_RES = {'F':0.25,'L':0.25,'M':0.25,'I':0.15,'V':0.1,'W':0.1,
+        'Y':0.1,'A':0.05,'R':0.1,'K':0.1,'H':0.05}
+M2_RES = {'R':0.35,'K':0.15,'H':0.05}
+
+@dataclass
 class fasta:
-    def __init__(self,desc,seq):
-        self.desc = desc
-        self.seq = seq
+    desc:str
+    seq:str
+
 def fasta_file(it): 
     """generator that returns an iterator over the records in a FASTA format file """
     d = fasta('','')
@@ -18,26 +27,19 @@ def fasta_file(it):
             d.seq += i.strip()
     if len(d.seq) > 0: 
         yield d
-hydRes = {'A':0.05,'F':0.2,'I':0.2,'L':0.2,'M':0.25,'V':0.1,'W':0.1,'Y':0.1}
-basicRes = {'R':0.1,'K':0.07,'H':0.04,'D':-0.06,'E':-0.06,'Y':0.0}
-M3Res = {'F':0.25,'L':0.25,'M':0.25,'I':0.15,'V':0.1,'W':0.1,
-        'Y':0.1,'A':0.05,'R':0.1,'K':0.1,'H':0.05}
-M2Res = {'R':0.35,'K':0.15,'H':0.05}
-class prbh_protein:
-    def __init__(self,fasta,prbhs):
-        self.fasta, self.prbhs = (fasta,prbhs)
+
 class prbh_motif:
     def __init__(self,bh,apkcs,score):
         self.bh, self.apkcs, self.score = (bh,apkcs,score)
-        def __eq__(self,other):
-            return self.bh == other.bh and (self.score -
-                 other.score) < 0.00001 and self.apkcs[0] == other.apkcs[0]
+    def __eq__(self,other):
+        return self.bh == other.bh and (self.score -
+                other.score) < 0.00001 and self.apkcs[0] == other.apkcs[0]
 class bh_motif:
     def __init__(self,start,end,height,area):
         self.start, self.end, self.height, self.area = (start,end,height,area)
-        def __eq__(self,other):
-            return self.start == other.start and (self.height - 
-                other.height) < 0.00001 and (self.area - other.area) < 0.00001
+    def __eq__(self,other):
+        return self.start == other.start and (self.height - 
+            other.height) < 0.00001 and (self.area - other.area) < 0.00001
 class aPKC_site:
     def __init__(self,pos,score,seq):
         self.pos, self.score, self.seq = (pos,score,seq)
@@ -55,18 +57,18 @@ def calc_aPKC_sites(seq, threshold=0.6):
             score = 0.0
             # pos + 1 hydrophobic if pos+1 < len(seq):
             if pos+1 < len(seq):
-                score += hydRes.get(seq[pos+1],-0.4)
+                score += HYD_RES.get(seq[pos+1],-0.4)
             basicTemp = 0.0
             for i in range(2,7):
                 if pos+i+1 < len(seq):
-                    basicTemp += basicRes.get(seq[pos+i],0.0)
+                    basicTemp += BASIC_RES.get(seq[pos+i],0.0)
             score += basicTemp * 0.8
             if pos-2 > 0:
-                score += M2Res.get(seq[pos-2],0.)*1.4
+                score += M2_RES.get(seq[pos-2],0.)*1.4
             if pos-3 > 0:
-                score += M3Res.get(seq[pos-3],0.)
-            hyd = max_seq(seq[max(0,pos-9):max(0,pos-4)],hydRes)
-            bas = max_seq(seq[max(0,pos-9):max(0,pos-4)],basicRes) 
+                score += M3_RES.get(seq[pos-3],0.)
+            hyd = max_seq(seq[max(0,pos-9):max(0,pos-4)],HYD_RES)
+            bas = max_seq(seq[max(0,pos-9):max(0,pos-4)],BASIC_RES) 
             score += (hyd + bas) * 0.5
             if score > threshold:
                 sites.append(aPKC_site(pos,score,seq[max(0,pos-
@@ -135,7 +137,6 @@ class tagged_prbh(object):
     def __hash__(self):
         return hash((self.prbh.score, self.prbh.apkcs[0].seq))
 def fasta_prbh(fasta): 
-    import gzip
     if fasta[-2:] == 'gz':
         fi = gzip.GzipFile(fasta,'r')
     else:
@@ -179,7 +180,7 @@ if __name__ == "main":
     parser = argparse.ArgumentParser(
                     prog='prbh',
                     description='implements the PhosphoRegulated Basic Hydrophobic (PRBH) motif finder as described in Bailey and Prehoda, Dev Cell (2018)')
-    parser.add_argument('fasta_file',help="File in FASTA format that contains protein sequences")
+    parser.add_argument('fasta_file',help="File in FASTA format (can be gzipped) that contains protein sequences")
     parser.add_argument('-o', '--output', help="Optional output file (otherwise output it written to stdout)")
     args = parser.parse_args()
     print(args)
